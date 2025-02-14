a84f230203dabb223c5c41a8774f70b3e3ce4525
Seu Nome <leandropacheco@xipptech.com.br>
2025-02-14 11:17:33 -0300

    Fix module structure

--- src/app.py
+++ src/app.py
@@ -3,21 +3,20 @@ from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app)  # Habilitando CORS para permitir requisições de diferentes origens
+CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "API is running"})
 
 @app.route('/cep/<cep>', methods=['GET'])
 def get_cep_info(cep):
-    # URL da API de consulta de CEP
     url = f'https://viacep.com.br/ws/{cep}/json/'
-    
-    # Fazendo a requisição para a API
     response = requests.get(url)
-    
-    # Verificando se a requisição foi bem-sucedida
     if response.status_code == 200:
         return jsonify(response.json())
     else:
         return jsonify({'error': 'CEP not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
